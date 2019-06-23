2906c17314f8de7bbf63b7019fdffe8c54d1ea08
Stanislava <stasya.gera@yandex.ru>
2019-06-23 12:17:27 +0300

    gave up on old structure

--- expression/generic/GenericExpression.java
+++
@@ -1,9 +0,0 @@
-package expression.generic;
-
-/**
- * Created by penguinni on 14.04.16.
- * penguinni hopes it will work.
- */
-public interface GenericExpression<Numeric> {
-     Numeric evaluate(int x, int y, int z);
-}
